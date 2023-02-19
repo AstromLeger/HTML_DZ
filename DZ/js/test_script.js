477fa446c24828e735bc387f8568bb68cc281ef4
Aleksei Vaskov <prizrak1143@gmail.com>
2023-02-19 13:58:36 +0300

    домашнее задание 3(2)

--- DZ/js/test_script.js
+++ DZ/js/test_script.js
@@ -1,6 +1,32 @@
-function Zapros(){
-let name=prompt("Как вас зовут? ")
-alert (`Привет,${ name}`);
+// Домашняя работа задача 1
+// function Zapros(){
+// let name=prompt("Как вас зовут? ")
+// alert (`Привет,${ name}`);
+// }
+
+// Zapros()
+
+// Домашняя работа 3 задание 2
+function ZaprosFilma(){
+let NameFilm = prompt("Название любимого фильма");
+let film1 = "Назад в будущее";
+let film2 = "Место встречи изменить нельзя";
+let film3 = "Москва слезам не верит";
+let film4 = "Звездный десант";
+
+
+    switch(NameFilm) {
+        case film1:  
+        break;
+        case film2:  
+        break;
+        case film3: 
+        break;
+        case film4: 
+        break;
+        default: alert('Попробуйте еще раз');
+    }
 }
 
-Zapros()
+
+ZaprosFilma()
